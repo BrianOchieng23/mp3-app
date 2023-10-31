adbc7d9773324dfcc7f9642cc155135c61c087ab
brianochieng23 <ochiengbr23@gmail.com>
2023-10-31 17:50:42 +0300

    creation of a mini mp3

---
+++ music.js
@@ -0,0 +1,53 @@
+//javascriptpro_
+let allmusic = [
+  {
+    name: 'where_is_the_love',
+    artist: 'black_eyed_peas',
+    img: 'image1',
+    src: 'music1'
+  },
+  {
+    name: 'something_just_like_this',
+    artist: 'chainsmokers_paris',
+    img: 'image2',
+    src: 'music2'
+  },
+  {
+    name: 'clouds',
+    artist: 'kayne',
+    img: 'image3',
+    src: 'music3'
+  },
+  {
+    name: 'coming_home',
+    artist: 'diddy_dirty_money ft._skylar_grey',
+    img: 'image4',
+    src: 'music4'
+  },
+  {
+    name: 'do_you_remember',
+    artist: 'jay_sean ft._sean_paul_lil_jon',
+    img: 'image5',
+    src: 'music5'
+  },
+  {
+    name: 'jireh_elevation_worship',
+    artist: 'maverick_city',
+    img: 'image6',
+    src: 'music6'
+  },
+  {
+    name: 'fast_car',
+    artist: 'jonas_blue ft._dakota',
+    img: 'image7',
+    src: 'music7'
+  },
+  {
+    name: 'high_on_life',
+    artist: 'martin_garrix_ft._bonn',
+    img: 'image8',
+    src: 'music8'
+  }
+]
+
+//You can add more songs like this
